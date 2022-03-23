867170c2931152953f3e122da8935b6758008f1b
Evgeny <ded@liis.su>
2022-03-24 00:13:59 +0300

    added logic to main button

--- learn_swift/ViewController.swift
+++ learn_swift/ViewController.swift
@@ -8,12 +8,26 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var mainTitle: UILabel!
+    
+    @IBOutlet var toggleButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        mainTitle.isHidden = true
+        toggleButton.layer.cornerRadius = 8
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func onButtonPress() {
+        mainTitle.isHidden = !mainTitle.isHidden
+        
+        let btnText = mainTitle.isHidden ? "Show Text" : "Hide Text" 
+        
+        toggleButton.setTitle(btnText, for: .normal)
+    }
+    
 }
 
